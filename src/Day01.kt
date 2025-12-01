88ab87b3e438114d803306582c4fe1b9702f6cb9
Stefan Roeck <stefan.roeck@gmail.com>
2025-12-01 07:04:54 +0100

    Day01, Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,35 @@
 fun main() {
+
+    data class PositionWithZeroCounter(val pos: Int, val zeros: Int = 0)
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val result = input.fold(PositionWithZeroCounter(50)) { dialPos, operation ->
+            val direction = operation.first()
+            val steps = operation.substring(1).toInt().rem(100)
+            val nextPos = when (direction) {
+                'R' -> (dialPos.pos + steps).rem(100)
+                'L' -> {
+                    (dialPos.pos - steps).let {
+                        if (it < 0) (100 + it) else it
+                    }
+                }
+
+                else -> error("Invalid direction: $direction")
+            }
+            dialPos.copy(pos = nextPos, zeros = if (nextPos == 0) dialPos.zeros + 1 else dialPos.zeros)
+        }
+        return result.zeros
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // Or read a large test input from the `src/Day01.txt` file:
+    check(part1(readInput("Day01_test")) == 3)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    //part2(input).println()
 }
