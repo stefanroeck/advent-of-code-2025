8e7748b893487de8bda0711a177256a5093db0bc
Stefan Roeck <stefan.roeck@gmail.com>
2025-12-01 07:39:09 +0100

    Day01, Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,35 +1,59 @@
+import kotlin.math.floor
+
 fun main() {
 
     data class PositionWithZeroCounter(val pos: Int, val zeros: Int = 0)
 
-    fun part1(input: List<String>): Int {
-        val result = input.fold(PositionWithZeroCounter(50)) { dialPos, operation ->
+    fun countZeros(input: List<String>, countTicks: Boolean = false): Int {
+        return input.fold(PositionWithZeroCounter(50)) { dialPos, operation ->
             val direction = operation.first()
-            val steps = operation.substring(1).toInt().rem(100)
+            var additionalTicks = 0
+            val steps = operation.substring(1).toInt().also {
+                additionalTicks += floor(it / 100.0).toInt()
+            }.rem(100)
             val nextPos = when (direction) {
-                'R' -> (dialPos.pos + steps).rem(100)
+                'R' -> (dialPos.pos + steps).also {
+                    if (it > 100 /* ignore 100*/) additionalTicks++
+                }.rem(100)
+
                 'L' -> {
                     (dialPos.pos - steps).let {
-                        if (it < 0) (100 + it) else it
+                        if (it < 0) {
+                            (100 + it).also {
+                                if (dialPos.pos != 0)
+                                    additionalTicks++
+                            }
+                        } else it
                     }
                 }
 
                 else -> error("Invalid direction: $direction")
             }
-            dialPos.copy(pos = nextPos, zeros = if (nextPos == 0) dialPos.zeros + 1 else dialPos.zeros)
-        }
-        return result.zeros
+            val newZeroCount = (if (nextPos == 0) dialPos.zeros + 1 else dialPos.zeros).let {
+                if (countTicks) it + additionalTicks else it
+            }
+            dialPos.copy(pos = nextPos, zeros = newZeroCount)
+        }.zeros
+    }
+
+    fun part1(input: List<String>): Int {
+        return countZeros(input, countTicks = false)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return countZeros(input, countTicks = true)
     }
 
-    // Or read a large test input from the `src/Day01.txt` file:
-    check(part1(readInput("Day01_test")) == 3)
+    fun compareAndCheck(actual: Int, expected: Int) {
+        if (actual != expected) {
+            println("ERROR! Expected=$expected, Actual=$actual")
+        }
+    }
 
-    // Read the input from the `src/Day01.txt` file.
+    compareAndCheck(part1(readInput("Day01_test")), 3)
+    compareAndCheck(part2(readInput("Day01_test")), 6)
+
     val input = readInput("Day01")
-    part1(input).println()
-    //part2(input).println()
+    part1(input).println() //1040
+    part2(input).println()
 }
