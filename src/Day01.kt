99ac9670249eaa0ebd349f178a6cf6f28496ec6e
Stefan Roeck <stefan.roeck@gmail.com>
2025-12-01 18:18:11 +0100

    Reformat with .editorconfig

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,59 +1,55 @@
 import kotlin.math.floor
 
+
 fun main() {
 
-    data class PositionWithZeroCounter(val pos: Int, val zeros: Int = 0)
+  data class PositionWithZeroCounter(val pos: Int, val zeros: Int = 0)
 
-    fun countZeros(input: List<String>, countTicks: Boolean = false): Int {
-        return input.fold(PositionWithZeroCounter(50)) { dialPos, operation ->
-            val direction = operation.first()
-            var additionalTicks = 0
-            val steps = operation.substring(1).toInt().also {
-                additionalTicks += floor(it / 100.0).toInt()
-            }.rem(100)
-            val nextPos = when (direction) {
-                'R' -> (dialPos.pos + steps).also {
-                    if (it > 100 /* ignore 100*/) additionalTicks++
-                }.rem(100)
+  fun countZeros(input: List<String>, countTicks: Boolean = false): Int {
+    return input.fold(PositionWithZeroCounter(50)) { dialPos, operation ->
+      val direction = operation.first()
+      var additionalTicks = 0
+      val steps = operation.substring(1).toInt().also {
+        additionalTicks += floor(it / 100.0).toInt()
+      }.rem(100)
+      
+      val nextPos = when (direction) {
+        'R' -> (dialPos.pos + steps).also {
+          if (it > 100 /* ignore 100*/) additionalTicks++
+        }.rem(100)
 
-                'L' -> {
-                    (dialPos.pos - steps).let {
-                        if (it < 0) {
-                            (100 + it).also {
-                                if (dialPos.pos != 0)
-                                    additionalTicks++
-                            }
-                        } else it
-                    }
-                }
-
-                else -> error("Invalid direction: $direction")
-            }
-            val newZeroCount = (if (nextPos == 0) dialPos.zeros + 1 else dialPos.zeros).let {
-                if (countTicks) it + additionalTicks else it
-            }
-            dialPos.copy(pos = nextPos, zeros = newZeroCount)
-        }.zeros
-    }
-
-    fun part1(input: List<String>): Int {
-        return countZeros(input, countTicks = false)
-    }
-
-    fun part2(input: List<String>): Int {
-        return countZeros(input, countTicks = true)
-    }
-
-    fun compareAndCheck(actual: Int, expected: Int) {
-        if (actual != expected) {
-            println("ERROR! Expected=$expected, Actual=$actual")
+        'L' -> {
+          (dialPos.pos - steps).let {
+            if (it < 0) {
+              (100 + it).also {
+                if (dialPos.pos != 0)
+                  additionalTicks++
+              }
+            } else it
+          }
         }
-    }
 
-    compareAndCheck(part1(readInput("Day01_test")), 3)
-    compareAndCheck(part2(readInput("Day01_test")), 6)
+        else -> error("Invalid direction: $direction")
+      }
+      val newZeroCount = (if (nextPos == 0) dialPos.zeros + 1 else dialPos.zeros).let {
+        if (countTicks) it + additionalTicks else it
+      }
+      dialPos.copy(pos = nextPos, zeros = newZeroCount)
+    }.zeros
+  }
 
-    val input = readInput("Day01")
-    part1(input).println() //1040
-    part2(input).println()
+  fun part1(input: List<String>): Int {
+    return countZeros(input, countTicks = false)
+  }
+
+  fun part2(input: List<String>): Int {
+    return countZeros(input, countTicks = true)
+  }
+
+  compareAndCheck(part1(readInput("Day01_test")), 3)
+  compareAndCheck(part2(readInput("Day01_test")), 6)
+
+  val input = readInput("Day01")
+  part1(input).println()
+  part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -12,10 +12,16 @@ fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+  .toString(16)
+  .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun compareAndCheck(actual: Int, expected: Int) {
+  if (actual != expected) {
+    println("ERROR! Expected=$expected, Actual=$actual")
+  }
+}
