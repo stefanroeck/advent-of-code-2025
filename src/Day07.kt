58f844f7a57d3cab518318632ac056a4f53914ae
Stefan Roeck <stefan.roeck@gmail.com>
2025-12-07 09:03:10 +0100

    Day07 Part 02

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,7 +1,5 @@
 fun main() {
-
-
-  fun processBeamDown(
+  fun processBeamDownPart1(
     map: MapOfPoints,
     position: ValuePoint,
     splitCount: Long,
@@ -20,44 +18,86 @@ fun main() {
 
     val leftBeam = map.pointAt(downBeam.x - 1, downBeam.y)
     val rightBeam = map.pointAt(downBeam.x + 1, downBeam.y)
-    println("Splitting at $downBeam")
 
     var additionalSplits = 0
     val splitCountLeft = leftBeam?.let {
       if (!beamStartingPoints.contains(it)) {
         additionalSplits = 1
         beamStartingPoints.add(it)
-        processBeamDown(map, it, splitCount, beamStartingPoints)
+        processBeamDownPart1(map, it, splitCount, beamStartingPoints)
       } else 0
     } ?: 0
     val splitCountRight = rightBeam?.let {
       if (!beamStartingPoints.contains(it)) {
         additionalSplits = 1
         beamStartingPoints.add(it)
-        processBeamDown(map, it, splitCount, beamStartingPoints)
+        processBeamDownPart1(map, it, splitCount, beamStartingPoints)
       } else 0
     } ?: 0
+
     return splitCountLeft + splitCountRight + additionalSplits
+  }
+
+
+  fun processBeamDownPart2(
+    map: MapOfPoints,
+    position: ValuePoint,
+    reachedBottomCount: Long,
+    splitterPointTimelineCount: MutableMap<ValuePoint, Long>,
+  ): Long {
+    var downBeam: ValuePoint? = position
+    do {
+      check(downBeam != null)
+      downBeam = map.pointAt(downBeam.x, downBeam.y + 1)
+    } while (downBeam != null && downBeam.value == ".")
+
+    if (downBeam == null) {
+      return 1
+    }
+    check(downBeam.value == "^")
+
+    splitterPointTimelineCount[downBeam]?.let {
+      return it
+    }
+
+    val leftBeam = map.pointAt(downBeam.x - 1, downBeam.y)
+    val rightBeam = map.pointAt(downBeam.x + 1, downBeam.y)
+
+    val timelineCountLeft = leftBeam?.let {
+      processBeamDownPart2(map, it, reachedBottomCount, splitterPointTimelineCount)
+    } ?: error("splitter at left border")
+    val timelineCountRight = rightBeam?.let {
+      processBeamDownPart2(map, it, reachedBottomCount, splitterPointTimelineCount)
+    } ?: error("splitter at right border")
+
+    (timelineCountLeft + timelineCountRight).let { timelineCount ->
+      splitterPointTimelineCount[downBeam] = timelineCount
+      return timelineCount
+    }
   }
 
   fun part1(input: List<String>): Long {
     val map = MapOfPoints(input, "")
     val start = map.pointsWithValue("S").single()
-    var beamStartingPoints = mutableSetOf<ValuePoint>()
+    val beamStartingPoints = mutableSetOf<ValuePoint>()
 
-    return processBeamDown(map, start, 0, beamStartingPoints)
+    return processBeamDownPart1(map, start, 0, beamStartingPoints)
   }
 
 
   fun part2(input: List<String>): Long {
-    return 0
+    val map = MapOfPoints(input, "")
+    val start = map.pointsWithValue("S").single()
+    val splitterPointCosts = mutableMapOf<ValuePoint, Long>()
+
+    return processBeamDownPart2(map, start, 0, splitterPointCosts)
   }
 
   compareAndCheck(part1(readInput("Day07_test")), 21)
-  //compareAndCheck(part2(readInput("Day07_test")), 3263827)
+  compareAndCheck(part2(readInput("Day07_test")), 40)
 
   val input = readInput("Day07")
   part1(input).println()
-  //part2(input).println()
+  part2(input).println()
 }
 
