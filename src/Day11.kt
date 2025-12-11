6ff725ae328bbf74faf394916a3c78c8b64c2c85
stefanroeck <stefan.roeck@dm.de>
2025-12-11 13:21:02 +0100

    Day 11 Part 02 start

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,43 +1,54 @@
+import java.util.concurrent.atomic.AtomicLong
+import java.util.function.Predicate
 
 
 fun main() {
 
-  fun findPathes(directions: Map<String, List<String>>, currentPath: List<String>, foundPathes: MutableSet<List<String>>) {
+  fun findPaths(directions: Map<String, List<String>>, currentPath: List<String>, foundPathes: AtomicLong, pathPredicate: Predicate<List<String>>) {
     val currentToken = currentPath.last()
     if (currentToken == "out") {
-      foundPathes.add(currentPath)
+      if (pathPredicate.test(currentPath)) {
+        foundPathes.incrementAndGet()
+      }
       return
     }
 
-    directions[currentToken]!!
-      .forEach { nextToken ->
-        if (!currentPath.contains(nextToken)){
-          findPathes(directions,  currentPath + nextToken, foundPathes)
-
+    directions[currentToken]!!.let {
+      it.forEach { nextToken ->
+        if (!currentPath.contains(nextToken)) {
+          findPaths(directions, currentPath + nextToken, foundPathes, pathPredicate)
         }
       }
+    }
   }
 
-  fun part1(input: List<String>): Long {
-    val directions = input.map { line ->
-      line.split(":").let { (first, second) ->
-        first to second.split(" ").filterNot { it.isEmpty() }
-      }
-    }.toMap()
+  fun parseInput(input: List<String>): Map<String, List<String>> = input.associate { line ->
+    line.split(":").let { (first, second) ->
+      first to second.split(" ").filterNot { it.isEmpty() }
+    }
+  }
 
-    val foundPaths = mutableSetOf<List<String>>()
-    findPathes(directions, listOf("you"), foundPaths)
-    return foundPaths.size.toLong();
+
+  fun part1(input: List<String>): Long {
+    val directions = parseInput(input)
+
+    val foundPaths = AtomicLong(0)
+    findPaths(directions, listOf("you"), foundPaths) {true}
+    return foundPaths.toLong();
   }
 
   fun part2(input: List<String>): Long {
-    return 0
+    val directions = parseInput(input)
+
+    val foundPaths = AtomicLong(0)
+    findPaths(directions, listOf("svr"), foundPaths) { it.contains("dac") && it.contains("fft") }
+    return foundPaths.toLong();
   }
 
   compareAndCheck(part1(readInput("Day11_test")), 5)
-  //compareAndCheck(part2(readInput("Day11_test")), 4174379265)
+  compareAndCheck(part2(readInput("Day11_test2")), 2)
 
   val input = readInput("Day11")
   part1(input).println()
-  //part2(input).println()
+  part2(input).println()
 }
